c77295adfb1d04b82e783aa12fd32e4812450f31
Borov777py <kozhushko756@mail.ru>
2025-03-09 19:01:51 +0300

    fix: check docker

--- db/storage.py
+++ db/storage.py
@@ -7,6 +7,7 @@ import os
 
 class Storage:
     redis = redis.Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], password=os.environ['REDIS_PASSWORD'], decode_responses=True)
+    redis.auth(password=os.environ['REDIS_PASSWORD'])
 
     @classmethod
     def get_data_messages(cls):
