cb1ff59822f7a37d083502f0070c0737ceae0914
Borov777py <kozhushko756@mail.ru>
2025-03-10 18:36:05 +0300

    fix: fixed messages service

--- services/messages.py
+++ services/messages.py
@@ -1,22 +1,36 @@
+import redis
+import json
+import os
+
 from schemas.messages import MessageAddSchema
-from db.storage import Storage
 
 
 class MessagesService:
-    storage = Storage
+    _key = 'messages'
+
+    _storage = redis.Redis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], password=os.environ['REDIS_PASSWORD'], decode_responses=True)
+    _storage.auth(password=os.environ['REDIS_PASSWORD'])
 
     @classmethod
     def add_new_message(cls, send_name: str, data: MessageAddSchema):
         message = f'{send_name}: {data.message}'
-        if cls.storage.add_message(message) is False:
-            return False
-        return True
+
+        if (messages := cls.get_messages()) is not None:
+            messages.append(message)
+            cls._storage.set(name=cls._key, value=json.dumps(messages))
+            return True
+        return False
 
     @classmethod
     def get_messages(cls):
-        if (messages := cls.storage.get_data_messages()) is not None:
-            return messages
-        return None
+        if (messages := cls._storage.get(name=cls._key)) is None:
+            cls._storage.set(name=cls._key, value=json.dumps([]))
+            cls.get_messages()
+        else:
+            try:
+                return json.loads(messages)
+            except:
+                return None
 
 
 
